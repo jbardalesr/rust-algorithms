1c7bf27905d1a42fe169e6923f7a8b14a8b8406c
jbardalesr <51254734+jbardalesr@users.noreply.github.com>
2020-11-27 16:34:21 -0500

    Create heapsort.rs

---
+++ Rust/heapsort.rs
@@ -0,0 +1,42 @@
+const LENGTH:usize = 17;
+struct Array {
+    list:[i32; LENGTH],
+    heap_size:usize
+}
+
+fn max_heapify(array:&mut Array, i:usize){
+    let l = 2*i;
+    let r = 2*i + 1;
+    let mut largest;
+
+    if l<=array.heap_size && array.list[l]>array.list[i] {
+        largest = l;
+    } else {
+        largest = i;
+    }
+
+    if r<=array.heap_size && array.list[r]>array.list[largest] {
+        largest = r;
+    }
+
+    if i != largest {
+        array.list.swap(largest, i);
+        max_heapify(array, largest);
+    }
+}
+
+fn build_max_heap(array: &mut Array){
+    array.heap_size = LENGTH - 1;
+    for i in (0..(LENGTH - 1)/2 + 1).rev() {
+        max_heapify(array, i);        
+    }
+}
+
+fn heapsort(array: &mut Array) {
+    build_max_heap(array);
+    for i in (1..LENGTH).rev() {
+        array.list.swap(0, i);
+        array.heap_size -= 1;
+        max_heapify(array, 0);
+    }
+}
